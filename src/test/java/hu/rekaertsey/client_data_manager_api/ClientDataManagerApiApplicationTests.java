db8145f8a16dec1666490cac493b93db005e6a38
Rekaert <rekaert@gmail.com>
2025-03-22 11:36:40 +0100

    initialize project

---
+++ src/test/java/hu/rekaertsey/client_data_manager_api/ClientDataManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.rekaertsey.client_data_manager_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientDataManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
